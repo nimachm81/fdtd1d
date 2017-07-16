64956d2fb01d99429dca09c778f7357adf221fd4
Nima Chamanara <nimachamanara@gmail.com>
2017-07-15 22:48:40 -0400

    CMake preparation..

---
+++ src/fdtd1d.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "fdtd1d.h"
+using namespace std;
+
+Student::Student(string name):name(name){}
+
+void Student::display(){
+	cout << "A student with name " << this->name << endl;
+}
+
---
+++ src/mainapp.cpp
@@ -0,0 +1,7 @@
+#include"fdtd1d.h"
+
+int main(int argc, char *argv[]){
+   Student s("Joe");
+   s.display();
+   return 0;
+}
